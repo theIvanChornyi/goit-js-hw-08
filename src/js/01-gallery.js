5bc4188d68fde897f2dc569b2b79fbb72b415d09
Ivan Chornyi <chorniyivan@icloud.com>
2022-08-27 13:07:00 +0300

    first task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,60 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+
+const galleryPhotoContainer = document.querySelector('.gallery');
+
+createGallery(galleryItems, galleryPhotoContainer);
+
+let lightbox = new SimpleLightbox('.gallery a',
+{
+  captions: true,
+  captionType: 'attr',
+  captionsData:	'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  docClose: true,
+});
+
+
+function createGallery(galleryItemsArray, galleryContainer) {
+
+  const galleryMarkupString = getMarkupString(galleryItemsArray);
+
+  createGalleryEl(galleryMarkupString, galleryContainer);
+
+  galleryContainer.addEventListener("click", zoomPicture);
+}
+
+
+
+
+function getMarkupString(galleryItemsArray) {
+  const MarkupString = galleryItemsArray
+  .map(
+    ({ preview, original, description }) =>
+      `<a class="gallery__link gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>`
+  )
+  .join("");
+  return MarkupString;
+}
+
+function createGalleryEl(markupString, galleryContainer) {
+  galleryContainer.innerHTML = markupString;
+}
+
+function zoomPicture(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
+  lightbox.on('show.simplelightbox');
+
+}
